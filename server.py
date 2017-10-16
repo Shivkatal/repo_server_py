70cffdfcf92ccebad8cb298cebfc51ddff3e369d
Shivkatal <sunny.ahlawat@students.iiit.ac.in>
2017-10-16 15:48:11 +0530

    handling choice

--- server.py
+++ server.py
@@ -13,11 +13,17 @@ serversocket.bind((host, port))
 
 #queue upto 5 requests
 serversocket.listen(5)
-
 while True:
-	#establish connection
+	flag=1
 	clientsocket, addr =  serversocket.accept()
 	print "Got a connection from %s" % str(addr)
 	currentTIme = time.ctime(time.time())+"\r\n"
 	clientsocket.send(currentTIme.encode('ascii'))
+	while flag:
+		#establish connection
+		choice = clientsocket.recv(10)
+		print choice
+		if choice != '1':
+			flag = 0
+
 	clientsocket.close()
